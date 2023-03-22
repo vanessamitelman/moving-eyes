18c71e2d2a26f950d23689d8cf4a672e326a4ed6
Vanessa Mitelman <vanessamitelman@gmail.com>
2023-03-22 21:09:48 +0200

    cleanup

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('mousemove', (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+  const anchor = document.getElementById('anchor');
+  const rekt = anchor.getBoundingClientRect();
+  const anchorX = rekt.left + rekt.width / 2;
+  const anchorY = rekt.top + rekt.height / 2;
+  const angleDeg = 180 - angle(mouseX, mouseY, anchorX, anchorY);
+  const eyes = document.querySelectorAll('.eye');
+  eyes.forEach((eye) => {
+    eye.style.transform = `rotate(${angleDeg}deg)`;
+  });
+});
+
+function angle(cx, cy, ex, ey) {
+  const dy = ey - cy;
+  const dx = ex - cx;
+  const rad = Math.atan2(dy, dx);
+  const deg = (rad * 180) / Math.PI;
+  return deg;
+}
